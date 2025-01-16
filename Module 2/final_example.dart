6962f82988a4fe0298ccc554971e4855affe3645
enji-coder <kailash.solanki@tops-int.com>
2025-01-16 15:47:35 +0530

    update

---
+++ Module 2/final_example.dart
@@ -0,0 +1,8 @@
+void main() {
+  final int age = 25; // 'age' is final, so it can't be changed once assigned.
+  
+  print(age); // Output: 25
+  
+  // Uncommenting the following line will cause an error:
+  // age = 30; // Error: Can't assign to the final variable 'age'.
+}
